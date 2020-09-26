a0d808dfc257a0278d534b7736c51d59f0135614
Jason Platts <plattsjm@icloud.com>
2020-09-26 15:35:56 -0400

    Added import from definition file.

---
+++ Definitions/layout.js
@@ -0,0 +1,26 @@
+var classes = [
+  {
+    label:"container",
+    kind:"Class",
+    detail:"A component for fixing an element's width to the current breakpoint.",
+    documentation:"The .container class sets the max-width of an element to match the min-width of the current breakpoint. This is useful if you'd prefer to design for a fixed set of screen sizes instead of trying to accommodate a fully fluid viewport.", 
+    filterText:"",
+    insertText:"",
+    range:"",
+    commitChars:"",
+    tokenize:false 
+  },
+  {
+    label:"px",
+    kind:"Class",
+    detail:"A component for fixing an element's width to the current breakpoint.",
+    documentation:"The .container class sets the max-width of an element to match the min-width of the current breakpoint. This is useful if you'd prefer to design for a fixed set of screen sizes instead of trying to accommodate a fully fluid viewport.", 
+    filterText:"",
+    insertText:"",
+    range:"",
+    commitChars:"",
+    tokenize:false 
+  },
+]
+
+exports.classes = classes;
--- Scripts/main.js
+++ Scripts/main.js
@@ -9,34 +9,39 @@ exports.deactivate = function() {
 
 
 class CompletionProvider {
-    constructor() {
+  constructor() {
+    
+    this.layout = require ('../Definitions/layout.js');
+    //console.log("con");
+  }
+  
+  provideCompletionItems(editor, context) {
+    // The text immediately preceding the cursor
+    //let text = context.text;
+    
+    // console.log(this.layout.classes[1].label);
+    
+    let items = [];
+    
+    for (let i = 0; i < this.layout.classes.length; i++) {
+        let cssClass = this.layout.classes[i];
         
+        let item = new CompletionItem(cssClass.label, CompletionItemKind.StyleClass);
+        
+        item.detail = "Layout Class"
+        item.documentation = "A component for fixing an element's width to the current breakpoint."
+        // The text to be inserted in the editor
+        //item.insertText = "<" + tag + ">$[]</" + tag + ">";
+        item.insertText = cssClass.label
+        
+        // Tokenize placeholders such as "$[]"
+        item.tokenize = true;
+        
+        items.push(item);
     }
     
-    provideCompletionItems(editor, context) {
-        // The text immediately preceding the cursor
-        let text = context.text;
-        
-        let tags = ["foo", "bar", "baz"];
-        let items = [];
-        
-        for (let i = 0; i < tags.length; i++) {
-            let tag = tags[i];
-            
-            let item = new CompletionItem(tag, CompletionItemKind.Tag);
-            
-            item.documentation = "This is my extension for TailwindCSS."
-            // The text to be inserted in the editor
-            item.insertText = "<" + tag + ">$[]</" + tag + ">";
-            
-            // Tokenize placeholders such as "$[]"
-            item.tokenize = true;
-            
-            items.push(item);
-        }
-        
-        return items;
-    }
+    return items;
+  }
 }
 
 
