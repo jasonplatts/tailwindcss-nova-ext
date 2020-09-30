1132cf204ceffa219c7d04abd0ea4402085f72a4
Jason Platts <plattsjm@icloud.com>
2020-09-30 19:03:17 -0400

    Fixed incorrect autocomple of hyphenated classes.
    Function returning current word entry range was added, which  is used to identify the range of the class name being currently entered by the user, including hyphens.
    
    The problem it solves is that an autocomplete will insert the entire label and will not replace any of class name before a hyphen. For example: If a user types “bg-re” and then uses the autocomplete to select “bg-red-100”, the inserted class will be “bg-bg-red-100”.

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -17,4 +17,27 @@ var convertHexToRgbColorObject = function(hex) {
   return convertRgbToColorObject(rgbArray);
 }
 
+var getRangeOfCurrentWord = function(editor, context) {
+  /*
+    The purpose of this function is to correctly identify the range of the class name being entered by
+    the user, including hyphens.
+    
+    The problem it solves is that an autocomplete will insert the entire label and will not replace any
+    of the class name typed before a hyphen because Nova considers it a separate word. For example: If
+    a user types “bg-re” and then uses the autocomplete to select “bg-red-100”, the inserted class will
+    be “bg-bg-red-100”.
+  */
+  let lineWordArray;
+  let lastWordBeforeCursor;
+  
+  lineWordArray = context.line.split(" ");
+  
+  if (lineWordArray.length > 0) {
+    lastWordBeforeCursor = lineWordArray[lineWordArray.length - 1];
+  };
+  
+  return new Range(context.position - lastWordBeforeCursor.length, context.position);
+}
+
 exports.convertHexToRgbColorObject = convertHexToRgbColorObject;
+exports.getRangeOfCurrentWord = getRangeOfCurrentWord;
--- Scripts/main.js
+++ Scripts/main.js
@@ -16,43 +16,15 @@ class CompletionProvider {
   }
   
   provideCompletionItems(editor, context) {
+    const FUNCTIONS = require('../Scripts/functions.js');
+    let currentWordRange = FUNCTIONS.getRangeOfCurrentWord(editor, context);
+    
     let items = [];
-    
-    // TRYING TO:
-    // Replace entered text with entire label
-    // SO:
-    // Need range of all entered text so far so can replace it with entire label
-    // WHICH IS:
-    // Document text range(?,context.position)
-    // ? IS:
-    // Document index of preceding space
-    // HOW DO WE GET THAT?
-    // get a string of word before cursor.
-    console.clear();
-    console.log("position:",context.position);
-    // Gets last Character Entered
-    console.log("text:",context.text);
-
-    var lineWordArray = context.line.split(" ");
-    console.log("lineWordArray:",lineWordArray);
-    console.log("wordCountinline:",lineWordArray.length);
-    
-    if (lineWordArray.length > 0) {
-      var lastWordBeforeCursor = lineWordArray[lineWordArray.length - 1];
-    };
-    
-    console.log("lastwordbeforecursor:",lastWordBeforeCursor);
-    
-    // REPLACE THIS RANGE WITH LABEL
-    let myRange = new Range(context.position - lastWordBeforeCursor.length, context.position);
-    console.log("myrangeindocument(lastchar):", editor.document.getTextInRange(myRange));
-    
     
     for (let fileCount = 0; fileCount < this.imports.length; fileCount++) {
       for (let i = 0; i < this.imports[fileCount].classes.length; i++) {
         let definitionObject = this.imports[fileCount].classes[i];
         
-        // Label and kind are passed to the CompletionItem object when instantiated.
         if (definitionObject.hasOwnProperty('color')) {
           var item = new CompletionItem(definitionObject.label, CompletionItemKind.Color);
         } else {
@@ -62,13 +34,8 @@ class CompletionProvider {
         if (definitionObject.hasOwnProperty('color')) { item.color = definitionObject.color; };
         if (definitionObject.hasOwnProperty('detail')) { item.detail = definitionObject.detail; };
         if (definitionObject.hasOwnProperty('documentation')) { item.documentation = definitionObject.documentation; };
-        if (definitionObject.hasOwnProperty('filterText')) { item.filterText = definitionObject.filterText; };
-        if (definitionObject.hasOwnProperty('insertText')) { item.insertText = definitionObject.insertText; };
-        if (definitionObject.hasOwnProperty('insertTextFormat')) {
-          item.insertTextFormat = definitionObject.insertTextFormat;
-        };
-        if (definitionObject.hasOwnProperty('range')) { item.range = definitionObject.range; };
-        if (definitionObject.hasOwnProperty('commitChars')) { item.commitChars = definitionObject.commitChars; };
+        
+        item.range = currentWordRange;
         
         items.push(item);
       }
