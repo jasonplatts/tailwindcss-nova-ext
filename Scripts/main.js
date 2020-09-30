755a7d0026f704cdcbc87868038685edd6d72beb
Jason Platts <plattsjm@icloud.com>
2020-09-30 16:51:40 -0400

    Added code to get range of entered text for replacement.

--- Scripts/main.js
+++ Scripts/main.js
@@ -18,14 +18,35 @@ class CompletionProvider {
   provideCompletionItems(editor, context) {
     let items = [];
     
-    let myText = editor.getTextInRange(new Range(0, editor.document.length));
+    // TRYING TO:
+    // Replace entered text with entire label
+    // SO:
+    // Need range of all entered text so far so can replace it with entire label
+    // WHICH IS:
+    // Document text range(?,context.position)
+    // ? IS:
+    // Document index of preceding space
+    // HOW DO WE GET THAT?
+    // get a string of word before cursor.
+    console.clear();
+    console.log("position:",context.position);
+    // Gets last Character Entered
+    console.log("text:",context.text);
+
+    var lineWordArray = context.line.split(" ");
+    console.log("lineWordArray:",lineWordArray);
+    console.log("wordCountinline:",lineWordArray.length);
     
-    // Gets index of previous space.
-    // let lastSpaceIndex = myText.lastIndexOf(' ', context.position);
-    // console.log(myText)
-    // console.log(myText.substr(myText.lastIndexOf(' '), context.position));
-    // console.log("last w, position, doc l: ",lastSpaceIndex + ", " + context.position + ", " + myText.length);
-    // console.log(myText.substr(lastSpaceIndex, parseInt(context.position)));
+    if (lineWordArray.length > 0) {
+      var lastWordBeforeCursor = lineWordArray[lineWordArray.length - 1];
+    };
+    
+    console.log("lastwordbeforecursor:",lastWordBeforeCursor);
+    
+    // REPLACE THIS RANGE WITH LABEL
+    let myRange = new Range(context.position - lastWordBeforeCursor.length, context.position);
+    console.log("myrangeindocument(lastchar):", editor.document.getTextInRange(myRange));
+    
     
     for (let fileCount = 0; fileCount < this.imports.length; fileCount++) {
       for (let i = 0; i < this.imports[fileCount].classes.length; i++) {
@@ -38,15 +59,16 @@ class CompletionProvider {
           var item = new CompletionItem(definitionObject.label, CompletionItemKind.StyleClass);
         }
         
-        
-        if (definitionObject.hasOwnProperty('color')) { item.color = definitionObject.color };
-        if (definitionObject.hasOwnProperty('detail')) { item.detail = definitionObject.detail };
-        if (definitionObject.hasOwnProperty('documentation')) { item.documentation = definitionObject.documentation };
-        if (definitionObject.hasOwnProperty('filterText')) { item.filterText = definitionObject.filterText };
-        if (definitionObject.hasOwnProperty('insertText')) { item.insertText = definitionObject.insertText };
-        if (definitionObject.hasOwnProperty('range')) { item.range = definitionObject.range };
-        if (definitionObject.hasOwnProperty('commitChars')) { item.commitChars = definitionObject.commitChars };
-        if (definitionObject.hasOwnProperty('tokenize')) { item.tokenize = definitionObject.tokenize };
+        if (definitionObject.hasOwnProperty('color')) { item.color = definitionObject.color; };
+        if (definitionObject.hasOwnProperty('detail')) { item.detail = definitionObject.detail; };
+        if (definitionObject.hasOwnProperty('documentation')) { item.documentation = definitionObject.documentation; };
+        if (definitionObject.hasOwnProperty('filterText')) { item.filterText = definitionObject.filterText; };
+        if (definitionObject.hasOwnProperty('insertText')) { item.insertText = definitionObject.insertText; };
+        if (definitionObject.hasOwnProperty('insertTextFormat')) {
+          item.insertTextFormat = definitionObject.insertTextFormat;
+        };
+        if (definitionObject.hasOwnProperty('range')) { item.range = definitionObject.range; };
+        if (definitionObject.hasOwnProperty('commitChars')) { item.commitChars = definitionObject.commitChars; };
         
         items.push(item);
       }
