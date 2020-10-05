eeec3c983e3cc90efe1544859071c253d86ef29a
Jason Platts <plattsjm@icloud.com>
2020-10-05 15:00:50 -0400

    Added flex direction, flex wrap, and flex classes.

--- Definitions/flexbox.js
+++ Definitions/flexbox.js
@@ -6,14 +6,68 @@ let flexDirection = [
   },
   {
     label:"flex-row-reverse",
-    detail:"flex-direction: row-reverse;"
+    detail:"flex-direction: row-reverse;",
+    documentation:"Positions flex items horizontally in the opposite direction."
+  },
+  {
+    label:"flex-col",
+    detail:"flex-direction: column;",
+    documentation:"Positions flex items vertically."
+  },
+  {
+    label:"flex-col-reverse",
+    detail:"flex-direction: column-reverse;",
+    documentation:"Positions flex items vertically in the opposite direction."
   }
-]
+];
+
+let flexWrap = [
+  {
+    label:"flex-wrap",
+    detail:"flex-wrap: wrap;",
+    documentation:"Allows flex items to wrap."
+  },
+  {
+    label:"flex-wrap-reverse",
+    detail:"flex-wrap: wrap-reverse;",
+    documentation:"Wraps flex items in the reverse direction."
+  },
+  {
+    label:"flex-no-wrap",
+    detail:"flex-wrap: nowrap;",
+    documentation:"Prevents flex items from wrapping, causing items to overflow their container when necessary."
+  }
+];
+
+let flex = [
+  {
+    label:"flex-1",
+    detail:"flex: 1 1 0%;",
+    documentation:"Allows a flex item to grow and shrink as needed. Initial element size is ignored."
+  },
+  {
+    label:"flex-auto",
+    detail:"flex: 1 1 auto;",
+    documentation:"Allows a flex item to grow and shrink. Initial element size is considered."
+  },
+  {
+    label:"flex-initial",
+    detail:"flex: 0 1 auto;",
+    documentation:"Allows a flex item to shrink, but not grow. Initial element size is considered."
+  },
+  {
+    label:"flex-none",
+    detail:"flex: none;",
+    documentation:"Prevents a flex item from growing or shrinking."
+  }
+];
 
 let classes = [];
 
 classes = classes.concat(
-  flexDirection
+  flexDirection,
+  flexWrap,
+  flex
 );
 
 exports.classes = classes;
