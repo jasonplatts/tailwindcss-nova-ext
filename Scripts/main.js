47c906dd8c747133d1042543558e40bdfa6575d8
Jason Platts <plattsjm@icloud.com>
2020-10-06 17:25:04 -0400

    Added todo comment in main.js for issue#4.

--- Scripts/main.js
+++ Scripts/main.js
@@ -35,9 +35,11 @@ class CompletionProvider {
         }
         
         if (definitionObject.hasOwnProperty('color')) { item.color = definitionObject.color; };
+        // TODO: Truncate long detail and documentation strings.
         if (definitionObject.hasOwnProperty('detail')) { item.detail = definitionObject.detail; };
         if (definitionObject.hasOwnProperty('documentation')) { item.documentation = definitionObject.documentation; };
         
+        // TODO: Account for completions of first class attribute entries and responsive class entries.
         item.range = currentWordRange;
         
         items.push(item);
