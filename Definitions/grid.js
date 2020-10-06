c85d131014ab33c7615f7a5c2d1168035fdce98e
Jason Platts <plattsjm@icloud.com>
2020-10-06 11:50:58 -0400

    Add grid column start end classes.

--- Definitions/grid.js
+++ Definitions/grid.js
@@ -1,3 +1,5 @@
+/*        GRID TEMPLATE COLUMNS CLASSES        */
+
 let gridTemplateColumns = [];
 
 for(i = 1; i <= 12; i++) {  
@@ -10,10 +12,81 @@ for(i = 1; i <= 12; i++) {
   );
 }
 
+gridTemplateColumns.push(
+  {
+    label:"grid-cols-none",
+    detail:"grid-template-columns: none;",
+    documentation:"Indicates no explicit grid column template."
+  }
+)
+
+/*        GRID COLUMN START/END CLASSES        */
+
+let gridColumnStartEnd = [];
+
+gridColumnStartEnd.push(
+  {
+    label:"col-auto",
+    detail:"grid-column: auto;",
+    documentation:"Indicates no explicit grid item placement or column span (default span of 1)."
+  }
+)
+
+for(i = 1; i <= 12; i++) {
+  gridColumnStartEnd.push(
+    {
+      label:"col-span-" + i,
+      detail:"grid-column: span " + i + " / span" + i + ";",
+      documentation:"Makes an element span " + i + " columns."
+    }
+  )
+}
+
+for(i = 1; i <= 13; i++) {
+  gridColumnStartEnd.push(
+    {
+      label:"col-start-" + i,
+      detail:"grid-column-start: " + i + ";",
+      documentation:"Makes an element start at column " + i + "."
+    }
+  )
+}
+
+gridColumnStartEnd.push(
+  {
+    label:"col-start-auto",
+    detail:"grid-column-start: auto;",
+    documentation:"Indicates no explicit grid item start placement."
+  }
+)
+
+for(i = 1; i <= 13; i++) {
+  gridColumnStartEnd.push(
+    {
+      label:"col-end-" + i,
+      detail:"grid-column-end: " + i + ";",
+      documentation:"Makes an element end at column " + i + "."
+    }
+  )
+}
+
+gridColumnStartEnd.push(
+  {
+    label:"col-end-auto",
+    detail:"grid-column-end: auto;",
+    documentation:"Indicates no explicit grid item end placement."
+  }
+)
+
+/*        GRID TEMPLATE ROWS CLASSES        */
+
+
+
 let classes = [];
 
 classes = classes.concat(
-  gridTemplateColumns
+  gridTemplateColumns,
+  gridColumnStartEnd
 );
 
 exports.classes = classes;
