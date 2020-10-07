17e7375b870e74f1940e824a0665bc0fdf1f8c06
Jason Platts <plattsjm@icloud.com>
2020-10-07 13:37:03 -0400

    Added spacing classes.

--- Definitions/spacing.js
+++ Definitions/spacing.js
@@ -144,10 +144,272 @@ padding.push(
   }
 );
 
+/*        MARGIN CLASSES        */
+
+let margin = [];
+
+margin.push(
+  {
+    label:"m-0",
+    detail:"margin: 0;",
+    documentation:"Set margin to 0 on all sides of an element."
+  }
+);
+
+for(i = 0; i < CONSTANTS.CLASS_REMS.length; i++) {
+  margin.push(
+    {
+      label:"m-" + CONSTANTS.CLASS_REMS[i].class,
+      detail:"margin: " + CONSTANTS.CLASS_REMS[i].value + "rem;",
+      documentation:"Set a margin of " + CONSTANTS.CLASS_REMS[i].value + "rem on all sides of an element."
+    },
+    {
+      label:"-m-" + CONSTANTS.CLASS_REMS[i].class,
+      detail:"margin: -" + CONSTANTS.CLASS_REMS[i].value + "rem;",
+      documentation:"Set a negative margin of " + CONSTANTS.CLASS_REMS[i].value + "rem on all sides of an element."
+    }
+  )
+}
+
+margin.push(
+  {
+    label:"m-auto",
+    detail:"margin: 1px;",
+    documentation:"Set the element's margin automatically, which is often used to center an element."
+  }
+);
+
+margin.push(
+  {
+    label:"m-px",
+    detail:"margin: 1px;",
+    documentation:"Set a margin of 1px on all sides of an element."
+  },
+  {
+    label:"-m-px",
+    detail:"margin: -1px;",
+    documentation:"Set a negative margin of -1px on all sides of an element."
+  }
+);
+
+margin.push(
+  {
+    label:"my-0",
+    detail:"margin-top: 0; margin-bottom: 0;",
+    documentation:"Set an element's top and bottom margin to 0."
+  },
+  {
+    label:"mx-0",
+    detail:"margin-left: 0; margin-right: 0;",
+    documentation:"Set an element's left and right margin to 0."
+  }
+);
+
+for(i = 0; i < CONSTANTS.CLASS_REMS.length; i++) {
+  margin.push(
+    {
+      label:"my-" + CONSTANTS.CLASS_REMS[i].class,
+      detail:"margin-top: " + CONSTANTS.CLASS_REMS[i].value + "rem;" + 
+        " margin-bottom: " + CONSTANTS.CLASS_REMS[i].value + "rem;",
+      documentation:"Set an element's top and bottom margin to " + CONSTANTS.CLASS_REMS[i].value + "rem."
+    },
+    {
+      label:"-my-" + CONSTANTS.CLASS_REMS[i].class,
+      detail:"margin-top: -" + CONSTANTS.CLASS_REMS[i].value + "rem;" + 
+        " margin-bottom: -" + CONSTANTS.CLASS_REMS[i].value + "rem;",
+      documentation:"Set an element's top and bottom margin to -" + CONSTANTS.CLASS_REMS[i].value + "rem."
+    },
+    {
+      label:"mx-" + CONSTANTS.CLASS_REMS[i].class,
+      detail:"margin-left: " + CONSTANTS.CLASS_REMS[i].value + "rem;" + 
+        " margin-right: " + CONSTANTS.CLASS_REMS[i].value + "rem;",
+      documentation:"Set an element's left and right margin to " + CONSTANTS.CLASS_REMS[i].value + "rem."
+    },
+    {
+      label:"-mx-" + CONSTANTS.CLASS_REMS[i].class,
+      detail:"margin-left: -" + CONSTANTS.CLASS_REMS[i].value + "rem;" + 
+        " margin-right: -" + CONSTANTS.CLASS_REMS[i].value + "rem;",
+      documentation:"Set an element's left and right margin to -" + CONSTANTS.CLASS_REMS[i].value + "rem."
+    }
+  );
+}
+
+margin.push(
+  {
+    label:"my-auto",
+    detail:"margin-top: auto; margin-bottom: auto;",
+    documentation:"Let the browser determine an element's top and bottom margin automatically."
+  },
+  {
+    label:"mx-auto",
+    detail:"margin-left: auto; margin-right: auto;",
+    documentation:"Let the browser determine an element's left and right margin automatically."
+  }
+);
+
+margin.push(
+  {
+    label:"my-px",
+    detail:"margin-top: 1px; margin-bottom: 1px;",
+    documentation:"Set an element's top and bottom margin to 1px."
+  },
+  {
+    label:"-my-px",
+    detail:"margin-top: -1px; margin-bottom: -1px;",
+    documentation:"Set an element's top and bottom margin to -1px."
+  },
+  {
+    label:"mx-px",
+    detail:"margin-left: 1px; margin-right: 1px;",
+    documentation:"Set an element's left and right margin to 1px."
+  },
+  {
+    label:"-mx-px",
+    detail:"margin-left: -1px; margin-right: -1px;",
+    documentation:"Set an element's left and right margin to -1px."
+  }
+);
+
+margin.push(
+  {
+    label:"mt-0",
+    detail:"margin-top: 0;",
+    documentation:"Set an element's top margin to 0."
+  },
+  {
+    label:"mr-0",
+    detail:"margin-right: 0;",
+    documentation:"Set an element's right margin to 0."
+  },
+  {
+    label:"mb-0",
+    detail:"margin-bottom: 0;",
+    documentation:"Set an element's bottom margin to 0."
+  },
+  {
+    label:"ml-0",
+    detail:"margin-left: 0;",
+    documentation:"Set an element's left margin to 0."
+  }
+);
+
+for(i = 0; i < CONSTANTS.CLASS_REMS.length; i++) {
+  margin.push(
+    {
+      label:"mt-" + CONSTANTS.CLASS_REMS[i].class,
+      detail:"margin-top: " + CONSTANTS.CLASS_REMS[i].value + "rem;",
+      documentation:"Set an element's top margin to " + CONSTANTS.CLASS_REMS[i].value + "rem."
+    },
+    {
+      label:"-mt-" + CONSTANTS.CLASS_REMS[i].class,
+      detail:"margin-top: -" + CONSTANTS.CLASS_REMS[i].value + "rem;",
+      documentation:"Set an element's top margin to -" + CONSTANTS.CLASS_REMS[i].value + "rem."
+    },
+    {
+      label:"mr-" + CONSTANTS.CLASS_REMS[i].class,
+      detail:"margin-right: " + CONSTANTS.CLASS_REMS[i].value + "rem;",
+      documentation:"Set an element's right margin to " + CONSTANTS.CLASS_REMS[i].value + "rem."
+    },
+    {
+      label:"-mr-" + CONSTANTS.CLASS_REMS[i].class,
+      detail:"margin-right: -" + CONSTANTS.CLASS_REMS[i].value + "rem;",
+      documentation:"Set an element's right margin to -" + CONSTANTS.CLASS_REMS[i].value + "rem."
+    },
+    {
+      label:"mb-" + CONSTANTS.CLASS_REMS[i].class,
+      detail:"margin-bottom: " + CONSTANTS.CLASS_REMS[i].value + "rem;",
+      documentation:"Set an element's bottom margin to " + CONSTANTS.CLASS_REMS[i].value + "rem."
+    },
+    {
+      label:"-mb-" + CONSTANTS.CLASS_REMS[i].class,
+      detail:"margin-bottom: -" + CONSTANTS.CLASS_REMS[i].value + "rem;",
+      documentation:"Set an element's bottom margin to -" + CONSTANTS.CLASS_REMS[i].value + "rem."
+    },
+    {
+      label:"ml-" + CONSTANTS.CLASS_REMS[i].class,
+      detail:"margin-left: " + CONSTANTS.CLASS_REMS[i].value + "rem;",
+      documentation:"Set an element's left margin to " + CONSTANTS.CLASS_REMS[i].value + "rem."
+    },
+    {
+      label:"-ml-" + CONSTANTS.CLASS_REMS[i].class,
+      detail:"margin-left: -" + CONSTANTS.CLASS_REMS[i].value + "rem;",
+      documentation:"Set an element's left margin to -" + CONSTANTS.CLASS_REMS[i].value + "rem."
+    }
+  );
+}
+
+margin.push(
+  {
+    label:"mt-auto",
+    detail:"margin-top: auto;",
+    documentation:"Let the browser determine an element's top margin automatically."
+  },
+  {
+    label:"mr-auto",
+    detail:"margin-right: auto;",
+    documentation:"Let the browser determine an element's right margin automatically."
+  },
+  {
+    label:"mb-auto",
+    detail:"margin-bottom: auto;",
+    documentation:"Let the browser determine an element's bottom margin automatically."
+  },
+  {
+    label:"ml-auto",
+    detail:"margin-left: auto;",
+    documentation:"Let the browser determine an element's left margin automatically."
+  }
+);
+
+margin.push(
+  {
+    label:"mt-px",
+    detail:"margin-top: 1px;",
+    documentation:"Set an element's top margin to 1px."
+  },
+  {
+    label:"-mt-px",
+    detail:"margin-top: -1px;",
+    documentation:"Set an element's top margin to -1px."
+  },
+  {
+    label:"mr-px",
+    detail:"margin-right: 1px;",
+    documentation:"Set an element's right margin to 1px."
+  },
+  {
+    label:"-mr-px",
+    detail:"margin-right: -1px;",
+    documentation:"Set an element's right margin to -1px."
+  },
+  {
+    label:"mb-px",
+    detail:"margin-bottom: 1px;",
+    documentation:"Set an element's bottom margin to 1px."
+  },
+  {
+    label:"-mb-px",
+    detail:"margin-bottom: -1px;",
+    documentation:"Set an element's bottom margin to -1px."
+  },
+  {
+    label:"ml-px",
+    detail:"margin-left: 1px;",
+    documentation:"Set an element's left margin to 1px."
+  },
+  {
+    label:"-ml-px",
+    detail:"margin-left: -1px;",
+    documentation:"Set an element's left margin to -1px."
+  }
+);
+
+
 let classes = [];
 
 classes = classes.concat(
- padding
+ padding,
+ margin
 );
 
 exports.classes = classes;
