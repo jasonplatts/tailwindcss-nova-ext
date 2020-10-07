46d4ee365a294fc05cad0254d13f1298b19efe31
Jason Platts <plattsjm@icloud.com>
2020-10-07 15:12:22 -0400

    Imported sizing class into main.js.

--- Scripts/main.js
+++ Scripts/main.js
@@ -16,6 +16,7 @@ class CompletionProvider {
     this.imports.push(require('../Definitions/grid.js'));
     this.imports.push(require('../Definitions/box-alignment.js'));
     this.imports.push(require('../Definitions/spacing.js'));
+    this.imports.push(require('../Definitions/sizing.js'));
     this.imports.push(require('../Definitions/backgrounds.js'));
   }
   
