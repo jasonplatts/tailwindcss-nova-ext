599c4d647d5349699975c76f6c11e3bad44b9dee
Jason Platts <plattsjm@icloud.com>
2020-10-07 18:29:25 -0400

    Added remaining text and background color classes.

--- Definitions/typography.js
+++ Definitions/typography.js
@@ -5,14 +5,28 @@ const FUNCTIONS = require('../Scripts/functions.js');
 
 let textColor = [];
 
+textColor.push(
+  {
+    label:"text-transparent",
+    color: new Color("rgb", [0, 0, 0, 0]),
+    detail:"color: transparent;",
+    documentation:"Set the text color of an element to transparent."
+  },
+  {
+    label:"text-current",
+    detail:"color: currentColor;",
+    documentation:"Set the text color of an element to the currentColor."
+  }
+);
+
 for(i = 0; i < COLORS.COLORS.length; i++) {  
   textColor.push(
     {
       label:"text-" + COLORS.COLORS[i].class,
       color: FUNCTIONS.convertHexToRgbColorObject(COLORS.COLORS[i].value),
       detail:"color: #" + COLORS.COLORS[i].value + ";",
-      documentation:"Set the text color an element to #" + COLORS.COLORS[i].value + "."
-    },
+      documentation:"Set the text color of an element to #" + COLORS.COLORS[i].value + "."
+    }
   );
 }
 
