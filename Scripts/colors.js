9f4d3969e9079144875be697ee5f9c85c0c16814
Jason Platts <plattsjm@icloud.com>
2020-10-07 18:12:04 -0400

    Added remaining colors.

--- Scripts/colors.js
+++ Scripts/colors.js
@@ -1,5 +1,23 @@
 let currentColorPrefix;
 
+currentColorPrefix = "black-";
+
+const BLACK = [
+  {
+    class:currentColorPrefix + "100",
+    value:"000"
+  }
+];
+
+currentColorPrefix = "white-";
+
+const WHITE = [
+  {
+    class:currentColorPrefix + "100",
+    value:"fff"
+  }
+];
+
 currentColorPrefix = "gray-";
 
 const GRAY = [
@@ -123,6 +141,295 @@ const ORANGE = [
   }
 ];
 
+currentColorPrefix = "yellow-";
+
+const YELLOW = [
+  {
+    class:currentColorPrefix + "100",
+    value:"fffff0"
+  },
+  {
+    class:currentColorPrefix + "200",
+    value:"fefcbf"
+  },
+  {
+    class:currentColorPrefix + "300",
+    value:"faf089"
+  },
+  {
+    class:currentColorPrefix + "400",
+    value:"f6e05e"
+  },
+  {
+    class:currentColorPrefix + "500",
+    value:"ecc94b"
+  },
+  {
+    class:currentColorPrefix + "600",
+    value:"d69e2e"
+  },
+  {
+    class:currentColorPrefix + "700",
+    value:"b7791f"
+  },
+  {
+    class:currentColorPrefix + "800",
+    value:"975a16"
+  },
+  {
+    class:currentColorPrefix + "900",
+    value:"744210"
+  }
+];
+
+currentColorPrefix = "green-";
+
+const GREEN = [
+  {
+    class:currentColorPrefix + "100",
+    value:"f0fff4"
+  },
+  {
+    class:currentColorPrefix + "200",
+    value:"c6f6d5"
+  },
+  {
+    class:currentColorPrefix + "300",
+    value:"9ae6b4"
+  },
+  {
+    class:currentColorPrefix + "400",
+    value:"68d391"
+  },
+  {
+    class:currentColorPrefix + "500",
+    value:"48bb78"
+  },
+  {
+    class:currentColorPrefix + "600",
+    value:"38a169"
+  },
+  {
+    class:currentColorPrefix + "700",
+    value:"2f855a"
+  },
+  {
+    class:currentColorPrefix + "800",
+    value:"276749"
+  },
+  {
+    class:currentColorPrefix + "900",
+    value:"22543d"
+  }
+];
+
+currentColorPrefix = "teal-";
+
+const TEAL = [
+  {
+    class:currentColorPrefix + "100",
+    value:"e6fffa"
+  },
+  {
+    class:currentColorPrefix + "200",
+    value:"b2f5ea"
+  },
+  {
+    class:currentColorPrefix + "300",
+    value:"81e6d9"
+  },
+  {
+    class:currentColorPrefix + "400",
+    value:"4fd1c5"
+  },
+  {
+    class:currentColorPrefix + "500",
+    value:"38b2ac"
+  },
+  {
+    class:currentColorPrefix + "600",
+    value:"319795"
+  },
+  {
+    class:currentColorPrefix + "700",
+    value:"2c7a7b"
+  },
+  {
+    class:currentColorPrefix + "800",
+    value:"285e61"
+  },
+  {
+    class:currentColorPrefix + "900",
+    value:"234e52"
+  }
+];
+
+currentColorPrefix = "blue-";
+
+const BLUE = [
+  {
+    class:currentColorPrefix + "100",
+    value:"ebf8ff"
+  },
+  {
+    class:currentColorPrefix + "200",
+    value:"bee3f8"
+  },
+  {
+    class:currentColorPrefix + "300",
+    value:"90cdf4"
+  },
+  {
+    class:currentColorPrefix + "400",
+    value:"63b3ed"
+  },
+  {
+    class:currentColorPrefix + "500",
+    value:"4299e1"
+  },
+  {
+    class:currentColorPrefix + "600",
+    value:"3182ce"
+  },
+  {
+    class:currentColorPrefix + "700",
+    value:"2b6cb0"
+  },
+  {
+    class:currentColorPrefix + "800",
+    value:"2c5282"
+  },
+  {
+    class:currentColorPrefix + "900",
+    value:"2a4365"
+  }
+];
+
+currentColorPrefix = "indigo-";
+
+const INDIGO = [
+  {
+    class:currentColorPrefix + "100",
+    value:"ebf4ff"
+  },
+  {
+    class:currentColorPrefix + "200",
+    value:"c3dafe"
+  },
+  {
+    class:currentColorPrefix + "300",
+    value:"a3bffa"
+  },
+  {
+    class:currentColorPrefix + "400",
+    value:"7f9cf5"
+  },
+  {
+    class:currentColorPrefix + "500",
+    value:"667eea"
+  },
+  {
+    class:currentColorPrefix + "600",
+    value:"5a67d8"
+  },
+  {
+    class:currentColorPrefix + "700",
+    value:"4c51bf"
+  },
+  {
+    class:currentColorPrefix + "800",
+    value:"434190"
+  },
+  {
+    class:currentColorPrefix + "900",
+    value:"3c366b"
+  }
+];
+
+currentColorPrefix = "purple-";
+
+const PURPLE = [
+  {
+    class:currentColorPrefix + "100",
+    value:"faf5ff"
+  },
+  {
+    class:currentColorPrefix + "200",
+    value:"e9d8fd"
+  },
+  {
+    class:currentColorPrefix + "300",
+    value:"d6bcfa"
+  },
+  {
+    class:currentColorPrefix + "400",
+    value:"b794f4"
+  },
+  {
+    class:currentColorPrefix + "500",
+    value:"9f7aea"
+  },
+  {
+    class:currentColorPrefix + "600",
+    value:"805ad5"
+  },
+  {
+    class:currentColorPrefix + "700",
+    value:"6b46c1"
+  },
+  {
+    class:currentColorPrefix + "800",
+    value:"553c9a"
+  },
+  {
+    class:currentColorPrefix + "900",
+    value:"44337a"
+  }
+];
+
+currentColorPrefix = "pink-";
+
+const PINK = [
+  {
+    class:currentColorPrefix + "100",
+    value:"fff5f7"
+  },
+  {
+    class:currentColorPrefix + "200",
+    value:"fed7e2"
+  },
+  {
+    class:currentColorPrefix + "300",
+    value:"fbb6ce"
+  },
+  {
+    class:currentColorPrefix + "400",
+    value:"f687b3"
+  },
+  {
+    class:currentColorPrefix + "500",
+    value:"ed64a6"
+  },
+  {
+    class:currentColorPrefix + "600",
+    value:"d53f8c"
+  },
+  {
+    class:currentColorPrefix + "700",
+    value:"b83280"
+  },
+  {
+    class:currentColorPrefix + "800",
+    value:"97266d"
+  },
+  {
+    class:currentColorPrefix + "900",
+    value:"702459"
+  }
+];
+
+currentColorPrefix = "color-";
+
 const TEMP = [
   {
     class:currentColorPrefix + "100",
@@ -165,9 +472,18 @@ const TEMP = [
 let colors = [];
 
 colors = colors.concat(
+  BLACK,
+  WHITE,
   GRAY,
   RED,
-  ORANGE
+  ORANGE,
+  YELLOW,
+  GREEN,
+  TEAL,
+  BLUE,
+  INDIGO,
+  PURPLE,
+  PINK
 );
 
 exports.COLORS = colors;
