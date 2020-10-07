190fb8e272e62bdd3608a10befeb7c34e5d4362a
Jason Platts <plattsjm@icloud.com>
2020-10-07 19:42:00 -0400

    Updated activationEvents and relevant test files.
    Added test files for php and erb. Updated extension.json to test for supported languages.

--- Tests/index.php
+++ Tests/index.php
@@ -5,10 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>title</title>
 </head>
-<%
-  # TODO: ERB COMPLETIONS NOT WORKING
-%>
+<?php
+  // TODO: PHP COMPLETIONS NOT WORKING
+?>
 <body class="text-red-200 bg-blue-100">
-  
+  <?php echo "Test Page"; ?>
 </body>
 </html>
