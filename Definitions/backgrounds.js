5162da625eff2cc02978166169a9268342d554ad
Jason Platts <plattsjm@icloud.com>
2020-10-09 13:19:19 -0400

    Added background attachment and clip classes.

--- Definitions/backgrounds.js
+++ Definitions/backgrounds.js
@@ -1,6 +1,51 @@
 const COLORS = require('../Scripts/colors.js');
 const FUNCTIONS = require('../Scripts/functions.js');
 
+/*        BACKGROUND ATTACHMENT CLASSES        */
+
+let backgroundAttachment = [
+  {
+    label:"bg-fixed",
+    detail:"background-attachment: fixed;",
+    documentation:"Fix the background image relative to the viewport."
+  },
+  {
+    label:"bg-local",
+    detail:"background-attachment: local;",
+    documentation:"Scroll the background image with the container and the viewport."
+  },
+  {
+    label:"bg-scroll",
+    detail:"background-attachment: scroll;",
+    documentation:"Scroll the background image with the viewport, but not with the container."
+  }
+];
+
+/*        BACKGROUND CLIP CLASSES        */
+
+let backgroundClip = [
+  {
+    label:"bg-clip-border",
+    detail:"background-clip: border-box;",
+    documentation:"Clip an element's background up to and including the border."
+  },
+  {
+    label:"bg-clip-local",
+    detail:"background-clip: padding-box;",
+    documentation:"Clip an element's background up to the inside edge of the border."
+  },
+  {
+    label:"bg-clip-scroll",
+    detail:"background-clip: content-box;",
+    documentation:"Clip an element's background up to the edge of the content box."
+  },
+  {
+    label:"bg-clip-text",
+    detail:"background-clip: text;",
+    documentation:"Crop an element's background to match the shape of the text."
+  }
+];
+
 /*        BACKGROUND COLOR CLASSES        */
 
 let backgroundColor = [];
@@ -33,7 +78,8 @@ for(i = 0; i < COLORS.COLORS.length; i++) {
 let classes = [];
 
 classes = classes.concat(
-  backgroundColor
+  backgroundColor,
+  backgroundAttachment
 );
 
 exports.classes = classes;
