6b2e4061d988ef452398e8707a4312cbc59603ce
Jason Platts <plattsjm@icloud.com>
2020-10-14 18:18:33 -0400

    Removed incorrect -rotate-0 class name.

--- Definitions/transforms.js
+++ Definitions/transforms.js
@@ -36,51 +36,41 @@ for(i = 0; i < SCALES.length; i++) {
 
 /*        ROTATE CLASSES        */
 
-const POSITIVE_ROTATIONS = [
-  { name:"0",   value:"0" },
-  { name:"1",   value:"1" },
-  { name:"2",   value:"2" },
-  { name:"3",   value:"3" },
-  { name:"6",   value:"6" },
-  { name:"12",  value:"12" },
-  { name:"45",  value:"45" },
-  { name:"90",  value:"90" },
-  { name:"180", value:"180" }
-];
-
-const NEGATIVE_ROTATIONS = [
-  { name:"180", value:"180" },
-  { name:"90",  value:"90" },
-  { name:"45",  value:"45" },
-  { name:"12",  value:"12" },
-  { name:"6",   value:"6" },
-  { name:"3",   value:"3" },
-  { name:"2",   value:"2" },
-  { name:"1",   value:"1" }
+const ROTATE_SCALE = [
+  { name:"1",   value:"1deg" },
+  { name:"2",   value:"2deg" },
+  { name:"3",   value:"3deg" },
+  { name:"6",   value:"6deg" },
+  { name:"12",  value:"12deg" },
+  { name:"45",  value:"45deg" },
+  { name:"90",  value:"90deg" },
+  { name:"180", value:"180deg" }
 ];
 
 let rotate = [];
 
-for(i = 0; i < POSITIVE_ROTATIONS.length; i++) {
+rotate.push(
+  {
+    label:`rotate-0`,
+    detail:`--transform-rotate: 0;`,
+    documentation:`Set an element's rotation angle to 0. The transform utility must first be used.`
+  }
+);
+
+for(i = 0; i < ROTATE_SCALE.length; i++) {
   rotate.push(
     {
-      label:`rotate-${POSITIVE_ROTATIONS[i].name}`,
-      detail:`--transform-rotate: ${POSITIVE_ROTATIONS[i].value}deg;`,
-      documentation:`Set an element's rotation angle to ${POSITIVE_ROTATIONS[i].value} degree. The transform utility must first be used.`
+      label:`rotate-${ROTATE_SCALE[i].name}`,
+      detail:`--transform-rotate: ${ROTATE_SCALE[i].value};`,
+      documentation:`Set an element's rotation angle to ${ROTATE_SCALE[i].value}. The transform utility must first be used.`
+    },
+    {
+      label:`-rotate-${ROTATE_SCALE[i].name}`,
+      detail:`--transform-rotate: -${ROTATE_SCALE[i].value};`,
+      documentation:`Set an element's rotation angle to -${ROTATE_SCALE[i].value}. The transform utility must first be used.`
     }
   );
 }
-
-for(i = 0; i < NEGATIVE_ROTATIONS.length; i++) {
-  rotate.push(
-    {
-      label:`-rotate-${NEGATIVE_ROTATIONS[i].name}`,
-      detail:`--transform-rotate: -${NEGATIVE_ROTATIONS[i].value}deg;`,
-      documentation:`Set an element's rotation angle to negative ${NEGATIVE_ROTATIONS[i].value} degree. The transform utility must first be used.`
-    }
-  );
-}
-
 
 /*        TRANSLATE CLASSES        */
 
