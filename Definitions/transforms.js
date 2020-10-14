f7471e0fe003ae0e54a2f1bc170fd4c195ec159f
Jason Platts <plattsjm@icloud.com>
2020-10-14 16:17:26 -0400

    Added rotate classes.

--- Definitions/transforms.js
+++ Definitions/transforms.js
@@ -34,10 +34,58 @@ for(i = 0; i < SCALES.length; i++) {
   )
 }
 
+/*        ROTATE CLASSES        */
+
+const POSITIVE_ROTATIONS = [
+  { name:"0",   value:"0" },
+  { name:"1",   value:"1" },
+  { name:"2",   value:"2" },
+  { name:"3",   value:"3" },
+  { name:"6",   value:"6" },
+  { name:"12",  value:"12" },
+  { name:"45",  value:"45" },
+  { name:"90",  value:"90" },
+  { name:"180", value:"180" }
+];
+
+const NEGATIVE_ROTATIONS = [
+  { name:"180", value:"180" },
+  { name:"90",  value:"90" },
+  { name:"45",  value:"45" },
+  { name:"12",  value:"12" },
+  { name:"6",   value:"6" },
+  { name:"3",   value:"3" },
+  { name:"2",   value:"2" },
+  { name:"1",   value:"1" }
+];
+
+let rotate = [];
+
+for(i = 0; i < POSITIVE_ROTATIONS.length; i++) {
+  rotate.push(
+    {
+      label:`rotate-${POSITIVE_ROTATIONS[i].name}`,
+      detail:`--transform-rotate: ${POSITIVE_ROTATIONS[i].value}deg;`,
+      documentation:`Set an element's rotation angle to ${POSITIVE_ROTATIONS[i].value} degree. The transform utility must first be used.`
+    }
+  );
+}
+
+for(i = 0; i < NEGATIVE_ROTATIONS.length; i++) {
+  rotate.push(
+    {
+      label:`-rotate-${NEGATIVE_ROTATIONS[i].name}`,
+      detail:`--transform-rotate: -${NEGATIVE_ROTATIONS[i].value}deg;`,
+      documentation:`Set an element's rotation angle to negative ${NEGATIVE_ROTATIONS[i].value} degree. The transform utility must first be used.`
+    }
+  );
+}
+
 let classes = [];
   
 classes = classes.concat(
-  scale
+  scale,
+  rotate
 );
 
 exports.classes = classes;
