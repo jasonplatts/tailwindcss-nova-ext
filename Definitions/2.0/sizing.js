3c48231b58a70ebef764ecd9dd78983728e23f4a
Jason Platts <plattsjm@icloud.com>
2021-01-19 16:54:56 -0500

    Updated sizing classes.

--- Definitions/2.0/sizing.js
+++ Definitions/2.0/sizing.js
@@ -2,113 +2,14 @@ const SCALES = require('./includes/scales.js');
 
 /*        WIDTH CLASSES        */
 
-const FRACTIONAL_WIDTH_SCALE = [
-  {
-    name:"1/2",
-    value:"50"
-  },
-  {
-    name:"1/3",
-    value:"33.333333"
-  },
-  {
-    name:"2/3",
-    value:"66.666667"
-  },
-  {
-    name:"1/4",
-    value:"25"
-  },
-  {
-    name:"2/4",
-    value:"50"
-  },
-  {
-    name:"3/4",
-    value:"75"
-  },
-  {
-    name:"1/5",
-    value:"20"
-  },
-  {
-    name:"2/5",
-    value:"40"
-  },
-  {
-    name:"3/5",
-    value:"60"
-  },
-  {
-    name:"4/5",
-    value:"80"
-  },
-  {
-    name:"1/6",
-    value:"16.666667"
-  },
-  {
-    name:"2/6",
-    value:"33.333333"
-  },
-  {
-    name:"3/6",
-    value:"50"
-  },
-  {
-    name:"4/6",
-    value:"66.666667"
-  },
-  {
-    name:"5/6",
-    value:"83.333333"
-  },
-  {
-    name:"1/12",
-    value:"8.333333"
-  },
-  {
-    name:"2/12",
-    value:"16.666667"
-  },
-  {
-    name:"3/12",
-    value:"25"
-  },
-  {
-    name:"4/12",
-    value:"33.333333"
-  },
-  {
-    name:"5/12",
-    value:"41.666667"
-  },
-  {
-    name:"6/12",
-    value:"50"
-  },
-  {
-    name:"7/12",
-    value:"58.333333"
-  },
-  {
-    name:"8/12",
-    value:"66.666667"
-  },
-  {
-    name:"9/12",
-    value:"75"
-  },
-  {
-    name:"10/12",
-    value:"83.333333"
-  },
-  {
-    name:"11/12",
-    value:"91.666667"
-  }
+const COMBINED_WIDTH_SCALE = [
+  ...SCALES.DEFAULT_SPACING_SCALE,
+  ...SCALES.CORE_DEFAULT_FRACTIONAL_SCALE,
+  ...SCALES.FIFTHS_FRACTIONAL_SCALE,
+  ...SCALES.SIXTHS_FRACTIONAL_SCALE,
+  ...SCALES.TWELFTHS_FRACTIONAL_SCALE
 ];
-
+   
 let width = [];
 
 width.push(
@@ -119,36 +20,31 @@ width.push(
   }
 );
 
-for(i = 0; i < SCALES.DEFAULT_SPACING_SCALE.length; i++) {
+for(i = 0; i < COMBINED_WIDTH_SCALE.length; i++) {
   width.push(
     {
-      label:`w-${SCALES.DEFAULT_SPACING_SCALE[i].name}`,
-      detail:`width: ${SCALES.DEFAULT_SPACING_SCALE[i].size};`,
-      documentation:`Set an element to a fixed width of ${SCALES.DEFAULT_SPACING_SCALE[i].size}.`
-    }
-  );
-}
-
-for(i = 0; i < FRACTIONAL_WIDTH_SCALE.length; i++) {
-  width.push(
-    {
-      label:"w-" + FRACTIONAL_WIDTH_SCALE[i].name,
-      detail:"width: " + FRACTIONAL_WIDTH_SCALE[i].value + "%;",
-      documentation:"Set an element to a fluid width of " + FRACTIONAL_WIDTH_SCALE[i].value + "%."
+      label:`w-${COMBINED_WIDTH_SCALE[i].name}`,
+      detail:`width: ${COMBINED_WIDTH_SCALE[i].value};`,
+      documentation:`Set the width of an element to ${COMBINED_WIDTH_SCALE[i].value}.`
     }
   );
 }
 
 width.push(
   {
-    label:"w-full",
-    detail:"width: 100%;",
-    documentation:"Set an element width to 100%."
-  },
-  {
     label:"w-screen",
     detail:"width: 100vw;",
     documentation:"Make an element span the entire width of the viewport."
+  },
+  {
+    label:"w-min",
+    detail:"width: min-content",
+    documentation:"Set an element width to the minimum content width."
+  },
+  {
+    label:"w-max",
+    detail:"width: max-content",
+    documentation:"Set an element width to the maximum content width."
   }
 );
 
@@ -166,93 +62,66 @@ minWidth.push(
     label:"min-w-full",
     detail:"min-width: 100%;",
     documentation:"Set the minimum element width to 100%."
+  },
+  {
+    label:"min-w-min",
+    detail:"min-width: min-content",
+    documentation:"Set the minimum width of an element to the minimum content width."
+  },
+  {
+    label:"min-w-max",
+    detail:"min-width: max-content",
+    documentation:"Set the minimum width of an element to the maximum content width."
   }
 );
 
 /*        MAX-WIDTH CLASSES        */
 
 const MAX_WIDTH_SCALE = [
-  {
-    name:"xs",
-    value:"20",
-  },
-  {
-    name:"sm",
-    value:"24",
-  },
-  {
-    name:"md",
-    value:"28",
-  },
-  {
-    name:"lg",
-    value:"32",
-  },
-  {
-    name:"xl",
-    value:"36",
-  },
-  {
-    name:"2xl",
-    value:"42",
-  },
-  {
-    name:"3xl",
-    value:"48",
-  },
-  {
-    name:"4xl",
-    value:"56",
-  },
-  {
-    name:"5xl",
-    value:"64",
-  },
-  {
-    name:"6xl",
-    value:"72",
-  }
+  { name:"0", value:"0rem" },
+  { name:"none", value:"none" },
+  { name:"xs", value:"20rem" },
+  { name:"sm", value:"24rem" },
+  { name:"md", value:"28rem" },
+  { name:"lg", value:"32rem" },
+  { name:"xl", value:"36rem" },
+  { name:"2xl", value:"42rem" },
+  { name:"3xl", value:"48rem" },
+  { name:"4xl", value:"56rem" },
+  { name:"5xl", value:"64rem" },
+  { name:"6xl", value:"72rem" },
+  { name:"7xl", value:"80rem" },
+  { name:"full", value:"100%" },
+  { name:"min", value:"min-content" },
+  { name:"max", value:"max-content" },
+  { name:"prose", value:"65ch" },
+  { name:"screen-sm", value:"640px" },
+  { name:"screen-md", value:"768px" },
+  { name:"screen-lg", value:"1024px" },
+  { name:"screen-xl", value:"1280px" },
+  { name:"screen-2xl", value:"1536px" }
 ];
 
 let maxWidth = [];
 
-maxWidth.push(
-  {
-    label:"max-w-none",
-    detail:"max-width: none;",
-    documentation:"Set the maximum element width to none."
-  }
-);
-
 for(i = 0; i < MAX_WIDTH_SCALE.length; i++) {
   maxWidth.push(
     {
-      label:"max-w-" + MAX_WIDTH_SCALE[i].name,
-      detail:"max-width: " + MAX_WIDTH_SCALE[i].value + "rem;",
-      documentation:"Set the maximum element width to " + MAX_WIDTH_SCALE[i].value + "rem."
-    }
-  );
-}
-
-maxWidth.push(
-  {
-    label:"max-w-full",
-    detail:"max-width: 100%;",
-    documentation:"Set the maximum element width to 100%."
-  }
-);
-
-for(i = 0; i < SCALES.BREAKPOINTS.length; i++) {
-  maxWidth.push(
-    {
-      label:"max-w-screen-" + SCALES.BREAKPOINTS[i].name,
-      detail:"max-width: " + SCALES.BREAKPOINTS[i].value + "px;",
-      documentation:"Set the maximum element width to " + SCALES.BREAKPOINTS[i].value + " pixels."
+      label:`max-w-${MAX_WIDTH_SCALE[i].name}`,
+      detail:`max-width: ${MAX_WIDTH_SCALE[i].value};`,
+      documentation:`Set the maximum element width to ${MAX_WIDTH_SCALE[i].value}.`
     }
   );
 }
 
 /*        HEIGHT CLASSES        */
+
+const COMBINED_HEIGHT_SCALE = [
+  ...SCALES.DEFAULT_SPACING_SCALE,
+  ...SCALES.CORE_DEFAULT_FRACTIONAL_SCALE,
+  ...SCALES.FIFTHS_FRACTIONAL_SCALE,
+  ...SCALES.SIXTHS_FRACTIONAL_SCALE
+];
 
 let height = [];
 
@@ -263,26 +132,23 @@ height.push(
     documentation:"Let the browser determine the height for an element."
   },
   {
-    label:"h-full",
-    detail:"height: 100%;",
-    documentation:"Set an element's height to 100% of its parent, as long as the parent has a defined height."
-  },
-  {
     label:"h-screen",
     detail:"height: 100vh;",
     documentation:"Make an element to span the entire height of the viewport."
-  },
+  }
 );
 
-for(i = 0; i < SCALES.DEFAULT_SPACING_SCALE.length; i++) {
+for(i = 0; i < COMBINED_HEIGHT_SCALE.length; i++) {
   height.push(
     {
-      label:`h-${SCALES.DEFAULT_SPACING_SCALE[i].name}`,
-      detail:`height: ${SCALES.DEFAULT_SPACING_SCALE[i].size};`,
-      documentation:`Set an element to a fixed height of ${SCALES.DEFAULT_SPACING_SCALE[i].size}.`
+      label:`h-${COMBINED_HEIGHT_SCALE[i].name}`,
+      detail:`height: ${COMBINED_HEIGHT_SCALE[i].value};`,
+      documentation:`Set an element's height to ${COMBINED_HEIGHT_SCALE[i].value}.`
     }
   );
 }
+
+/*        MIN-HEIGHT CLASSES        */
 
 let minHeight = [
   {
@@ -302,7 +168,21 @@ let minHeight = [
   }
 ];
 
-let maxHeight = [
+/*        MAX-HEIGHT CLASSES        */
+
+let maxHeight = [];
+
+for(i = 0; i < SCALES.DEFAULT_SPACING_SCALE.length; i++) {
+  height.push(
+    {
+      label:`max-h-${SCALES.DEFAULT_SPACING_SCALE[i].name}`,
+      detail:`max-height: ${SCALES.DEFAULT_SPACING_SCALE[i].value};`,
+      documentation:`Set the maximum height of an element to ${SCALES.DEFAULT_SPACING_SCALE[i].value}.`
+    }
+  );
+}
+
+maxHeight.push(
   {
     label:"max-h-full",
     detail:"max-height: 100%;",
@@ -313,7 +193,7 @@ let maxHeight = [
     detail:"max-height: 100vh;",
     documentation:"Set the maximum element height to span the entire height of the viewport."
   }
-];
+);
 
 let classes = [];
 
