5db11cb2d2a1a47a22ae414967cbdfdd6cfc621f
Jason Platts <plattsjm@icloud.com>
2021-07-07 10:12:42 -0400

    Updated Tailwind version number to 2.2.4.

--- Scripts/configuration.js
+++ Scripts/configuration.js
@@ -25,7 +25,7 @@ exports.Configuration = class Configuration {
     This is also the name of the directory containing the definition files.
   */
   static get VERSION() {
-    return '2.x'
+    return '2.2.4'
   }
 
   /*
