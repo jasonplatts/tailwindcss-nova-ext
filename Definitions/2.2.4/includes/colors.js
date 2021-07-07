6e5e54433a18e774a8d389754c1866c13f269530
Jason Platts <plattsjm@icloud.com>
2021-07-07 11:28:43 -0400

    Reformatted colors module.

--- Definitions/2.2.4/includes/colors.js
+++ Definitions/2.2.4/includes/colors.js
@@ -4,8 +4,8 @@ currentColorPrefix = 'black'
 
 const BLACK = [
   {
-    name:currentColorPrefix,
-    value:'000'
+    name:  currentColorPrefix,
+    value: '000'
   }
 ]
 
@@ -13,8 +13,8 @@ currentColorPrefix = 'white'
 
 const WHITE = [
   {
-    name:currentColorPrefix,
-    value:'fff'
+    name:  currentColorPrefix,
+    value: 'fff'
   }
 ]
 
@@ -22,44 +22,44 @@ currentColorPrefix = 'gray-'
 
 const GRAY = [
   {
-    name:currentColorPrefix + '50',
-    value:'F9FAFB'
+    name:  currentColorPrefix + '50',
+    value: 'F9FAFB'
   },
   {
-    name:currentColorPrefix + '100',
-    value:'F3F4F6'
+    name:  currentColorPrefix + '100',
+    value: 'F3F4F6'
   },
   {
-    name:currentColorPrefix + '200',
-    value:'E5E7EB'
+    name:  currentColorPrefix + '200',
+    value: 'E5E7EB'
   },
   {
-    name:currentColorPrefix + '300',
-    value:'D1D5DB'
+    name:  currentColorPrefix + '300',
+    value: 'D1D5DB'
   },
   {
-    name:currentColorPrefix + '400',
-    value:'9CA3AF'
+    name:  currentColorPrefix + '400',
+    value: '9CA3AF'
   },
   {
-    name:currentColorPrefix + '500',
-    value:'6B7280'
+    name:  currentColorPrefix + '500',
+    value: '6B7280'
   },
   {
-    name:currentColorPrefix + '600',
-    value:'4B5563'
+    name:  currentColorPrefix + '600',
+    value: '4B5563'
   },
   {
-    name:currentColorPrefix + '700',
-    value:'374151'
+    name:  currentColorPrefix + '700',
+    value: '374151'
   },
   {
-    name:currentColorPrefix + '800',
-    value:'1F2937'
+    name:  currentColorPrefix + '800',
+    value: '1F2937'
   },
   {
-    name:currentColorPrefix + '900',
-    value:'111827'
+    name:  currentColorPrefix + '900',
+    value: '111827'
   }
 ]
 
@@ -67,44 +67,44 @@ currentColorPrefix = 'red-'
 
 const RED = [
   {
-    name:currentColorPrefix + '50',
-    value:'FEF2F2'
+    name:  currentColorPrefix + '50',
+    value: 'FEF2F2'
   },
   {
-    name:currentColorPrefix + '100',
-    value:'FEE2E2'
+    name:  currentColorPrefix + '100',
+    value: 'FEE2E2'
   },
   {
-    name:currentColorPrefix + '200',
-    value:'FECACA'
+    name:  currentColorPrefix + '200',
+    value: 'FECACA'
   },
   {
-    name:currentColorPrefix + '300',
-    value:'FCA5A5'
+    name:  currentColorPrefix + '300',
+    value: 'FCA5A5'
   },
   {
-    name:currentColorPrefix + '400',
-    value:'F87171'
+    name:  currentColorPrefix + '400',
+    value: 'F87171'
   },
   {
-    name:currentColorPrefix + '500',
-    value:'EF4444'
+    name:  currentColorPrefix + '500',
+    value: 'EF4444'
   },
   {
-    name:currentColorPrefix + '600',
-    value:'DC2626'
+    name:  currentColorPrefix + '600',
+    value: 'DC2626'
   },
   {
-    name:currentColorPrefix + '700',
-    value:'B91C1C'
+    name:  currentColorPrefix + '700',
+    value: 'B91C1C'
   },
   {
-    name:currentColorPrefix + '800',
-    value:'991B1B'
+    name:  currentColorPrefix + '800',
+    value: '991B1B'
   },
   {
-    name:currentColorPrefix + '900',
-    value:'7F1D1D'
+    name:  currentColorPrefix + '900',
+    value: '7F1D1D'
   }
 ]
 
@@ -112,44 +112,44 @@ currentColorPrefix = 'yellow-'
 
 const YELLOW = [
   {
-    name:currentColorPrefix + '50',
-    value:'FFFBEB'
+    name:  currentColorPrefix + '50',
+    value: 'FFFBEB'
   },
   {
-    name:currentColorPrefix + '100',
-    value:'FEF3C7'
+    name:  currentColorPrefix + '100',
+    value: 'FEF3C7'
   },
   {
-    name:currentColorPrefix + '200',
-    value:'FDE68A'
+    name:  currentColorPrefix + '200',
+    value: 'FDE68A'
   },
   {
-    name:currentColorPrefix + '300',
-    value:'FCD34D'
+    name:  currentColorPrefix + '300',
+    value: 'FCD34D'
   },
   {
-    name:currentColorPrefix + '400',
-    value:'FBBF24'
+    name:  currentColorPrefix + '400',
+    value: 'FBBF24'
   },
   {
-    name:currentColorPrefix + '500',
-    value:'F59E0B'
+    name:  currentColorPrefix + '500',
+    value: 'F59E0B'
   },
   {
-    name:currentColorPrefix + '600',
-    value:'D97706'
+    name:  currentColorPrefix + '600',
+    value: 'D97706'
   },
   {
-    name:currentColorPrefix + '700',
-    value:'B45309'
+    name:  currentColorPrefix + '700',
+    value: 'B45309'
   },
   {
-    name:currentColorPrefix + '800',
-    value:'92400E'
+    name:  currentColorPrefix + '800',
+    value: '92400E'
   },
   {
-    name:currentColorPrefix + '900',
-    value:'78350F'
+    name:  currentColorPrefix + '900',
+    value: '78350F'
   }
 ]
 
@@ -157,44 +157,44 @@ currentColorPrefix = 'green-'
 
 const GREEN = [
   {
-    name:currentColorPrefix + '50',
-    value:'ECFDF5'
+    name:  currentColorPrefix + '50',
+    value: 'ECFDF5'
   },
   {
-    name:currentColorPrefix + '100',
-    value:'D1FAE5'
+    name:  currentColorPrefix + '100',
+    value: 'D1FAE5'
   },
   {
-    name:currentColorPrefix + '200',
-    value:'A7F3D0'
+    name:  currentColorPrefix + '200',
+    value: 'A7F3D0'
   },
   {
-    name:currentColorPrefix + '300',
-    value:'6EE7B7'
+    name:  currentColorPrefix + '300',
+    value: '6EE7B7'
   },
   {
-    name:currentColorPrefix + '400',
-    value:'34D399'
+    name:  currentColorPrefix + '400',
+    value: '34D399'
   },
   {
-    name:currentColorPrefix + '500',
-    value:'10B981'
+    name:  currentColorPrefix + '500',
+    value: '10B981'
   },
   {
-    name:currentColorPrefix + '600',
-    value:'059669'
+    name:  currentColorPrefix + '600',
+    value: '059669'
   },
   {
-    name:currentColorPrefix + '700',
-    value:'047857'
+    name:  currentColorPrefix + '700',
+    value: '047857'
   },
   {
-    name:currentColorPrefix + '800',
-    value:'065F46'
+    name:  currentColorPrefix + '800',
+    value: '065F46'
   },
   {
-    name:currentColorPrefix + '900',
-    value:'064E3B'
+    name:  currentColorPrefix + '900',
+    value: '064E3B'
   }
 ]
 
@@ -202,44 +202,44 @@ currentColorPrefix = 'blue-'
 
 const BLUE = [
   {
-    name:currentColorPrefix + '50',
-    value:'EFF6FF'
+    name:  currentColorPrefix + '50',
+    value: 'EFF6FF'
   },
   {
-    name:currentColorPrefix + '100',
-    value:'DBEAFE'
+    name:  currentColorPrefix + '100',
+    value: 'DBEAFE'
   },
   {
-    name:currentColorPrefix + '200',
-    value:'BFDBFE'
+    name:  currentColorPrefix + '200',
+    value: 'BFDBFE'
   },
   {
-    name:currentColorPrefix + '300',
-    value:'93C5FD'
+    name:  currentColorPrefix + '300',
+    value: '93C5FD'
   },
   {
-    name:currentColorPrefix + '400',
-    value:'60A5FA'
+    name:  currentColorPrefix + '400',
+    value: '60A5FA'
   },
   {
-    name:currentColorPrefix + '500',
-    value:'3B82F6'
+    name:  currentColorPrefix + '500',
+    value: '3B82F6'
   },
   {
-    name:currentColorPrefix + '600',
-    value:'2563EB'
+    name:  currentColorPrefix + '600',
+    value: '2563EB'
   },
   {
-    name:currentColorPrefix + '700',
-    value:'1D4ED8'
+    name:  currentColorPrefix + '700',
+    value: '1D4ED8'
   },
   {
-    name:currentColorPrefix + '800',
-    value:'1E40AF'
+    name:  currentColorPrefix + '800',
+    value: '1E40AF'
   },
   {
-    name:currentColorPrefix + '900',
-    value:'1E3A8A'
+    name:  currentColorPrefix + '900',
+    value: '1E3A8A'
   }
 ]
 
@@ -247,44 +247,44 @@ currentColorPrefix = 'indigo-'
 
 const INDIGO = [
   {
-    name:currentColorPrefix + '50',
-    value:'EEF2FF'
+    name:  currentColorPrefix + '50',
+    value: 'EEF2FF'
   },
   {
-    name:currentColorPrefix + '100',
-    value:'E0E7FF'
+    name:  currentColorPrefix + '100',
+    value: 'E0E7FF'
   },
   {
-    name:currentColorPrefix + '200',
-    value:'C7D2FE'
+    name:  currentColorPrefix + '200',
+    value: 'C7D2FE'
   },
   {
-    name:currentColorPrefix + '300',
-    value:'A5B4FC'
+    name:  currentColorPrefix + '300',
+    value: 'A5B4FC'
   },
   {
-    name:currentColorPrefix + '400',
-    value:'818CF8'
+    name:  currentColorPrefix + '400',
+    value: '818CF8'
   },
   {
-    name:currentColorPrefix + '500',
-    value:'6366F1'
+    name:  currentColorPrefix + '500',
+    value: '6366F1'
   },
   {
-    name:currentColorPrefix + '600',
-    value:'4F46E5'
+    name:  currentColorPrefix + '600',
+    value: '4F46E5'
   },
   {
-    name:currentColorPrefix + '700',
-    value:'4338CA'
+    name:  currentColorPrefix + '700',
+    value: '4338CA'
   },
   {
-    name:currentColorPrefix + '800',
-    value:'3730A3'
+    name:  currentColorPrefix + '800',
+    value: '3730A3'
   },
   {
-    name:currentColorPrefix + '900',
-    value:'312E81'
+    name:  currentColorPrefix + '900',
+    value: '312E81'
   }
 ]
 
@@ -292,44 +292,44 @@ currentColorPrefix = 'purple-'
 
 const PURPLE = [
   {
-    name:currentColorPrefix + '50',
-    value:'F5F3FF'
+    name:  currentColorPrefix + '50',
+    value: 'F5F3FF'
   },
   {
-    name:currentColorPrefix + '100',
-    value:'EDE9FE'
+    name:  currentColorPrefix + '100',
+    value: 'EDE9FE'
   },
   {
-    name:currentColorPrefix + '200',
-    value:'DDD6FE'
+    name:  currentColorPrefix + '200',
+    value: 'DDD6FE'
   },
   {
-    name:currentColorPrefix + '300',
-    value:'C4B5FD'
+    name:  currentColorPrefix + '300',
+    value: 'C4B5FD'
   },
   {
-    name:currentColorPrefix + '400',
-    value:'A78BFA'
+    name:  currentColorPrefix + '400',
+    value: 'A78BFA'
   },
   {
-    name:currentColorPrefix + '500',
-    value:'8B5CF6'
+    name:  currentColorPrefix + '500',
+    value: '8B5CF6'
   },
   {
-    name:currentColorPrefix + '600',
-    value:'7C3AED'
+    name:  currentColorPrefix + '600',
+    value: '7C3AED'
   },
   {
-    name:currentColorPrefix + '700',
-    value:'6D28D9'
+    name:  currentColorPrefix + '700',
+    value: '6D28D9'
   },
   {
-    name:currentColorPrefix + '800',
-    value:'5B21B6'
+    name:  currentColorPrefix + '800',
+    value: '5B21B6'
   },
   {
-    name:currentColorPrefix + '900',
-    value:'4C1D95'
+    name:  currentColorPrefix + '900',
+    value: '4C1D95'
   }
 ]
 
@@ -337,60 +337,56 @@ currentColorPrefix = 'pink-'
 
 const PINK = [
   {
-    name:currentColorPrefix + '50',
-    value:'FDF2F8'
+    name:  currentColorPrefix + '50',
+    value: 'FDF2F8'
   },
   {
-    name:currentColorPrefix + '100',
-    value:'FCE7F3'
+    name:  currentColorPrefix + '100',
+    value: 'FCE7F3'
   },
   {
-    name:currentColorPrefix + '200',
-    value:'FBCFE8'
+    name:  currentColorPrefix + '200',
+    value: 'FBCFE8'
   },
   {
-    name:currentColorPrefix + '300',
-    value:'F9A8D4'
+    name:  currentColorPrefix + '300',
+    value: 'F9A8D4'
   },
   {
-    name:currentColorPrefix + '400',
-    value:'F472B6'
+    name:  currentColorPrefix + '400',
+    value: 'F472B6'
   },
   {
-    name:currentColorPrefix + '500',
-    value:'EC4899'
+    name:  currentColorPrefix + '500',
+    value: 'EC4899'
   },
   {
-    name:currentColorPrefix + '600',
-    value:'DB2777'
+    name:  currentColorPrefix + '600',
+    value: 'DB2777'
   },
   {
-    name:currentColorPrefix + '700',
-    value:'BE185D'
+    name:  currentColorPrefix + '700',
+    value: 'BE185D'
   },
   {
-    name:currentColorPrefix + '800',
-    value:'9D174D'
+    name:  currentColorPrefix + '800',
+    value: '9D174D'
   },
   {
-    name:currentColorPrefix + '900',
-    value:'831843'
+    name:  currentColorPrefix + '900',
+    value: '831843'
   }
 ]
 
-let colors = []
-
-colors = colors.concat(
-  BLACK,
-  WHITE,
-  GRAY,
-  RED,
-  YELLOW,
-  GREEN,
-  BLUE,
-  INDIGO,
-  PURPLE,
-  PINK
-)
-
-exports.COLORS = colors
+exports.COLORS = [
+  ...BLACK,
+  ...WHITE,
+  ...GRAY,
+  ...RED,
+  ...YELLOW,
+  ...GREEN,
+  ...BLUE,
+  ...INDIGO,
+  ...PURPLE,
+  ...PINK
+]
