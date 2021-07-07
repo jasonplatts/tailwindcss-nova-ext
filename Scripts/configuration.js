80971f036ce2512cdbd9d2082bb8e84fc016ac04
Jason Platts <plattsjm@icloud.com>
2021-07-07 09:59:19 -0400

    Updated Configuration to allow manual sorting.

--- Scripts/configuration.js
+++ Scripts/configuration.js
@@ -7,9 +7,12 @@ const FUNCTIONS = require('./functions.js')
 */
 exports.Configuration = class Configuration {
   constructor() {
-    this._definition_files = []
-    this._definitions      = []
+    this._definitions = []
   }
+
+  /*
+    List of file types that should activate completions.
+  */
   static get SUPPORTED_FILE_TYPES() {
     return [
       'html', 'html+erb', 'html+eex', 'haml', 'php', 'blade', 'twig',
@@ -17,44 +20,58 @@ exports.Configuration = class Configuration {
     ]
   }
 
+  /*
+    The current version of Tailwind supported by the extension.
+    This is also the name of the directory containing the definition files.
+  */
   static get VERSION() {
     return '2.x'
   }
 
+  /*
+    List of definition files containing Tailwind classes.
+    Files are listen manually in order to set desired sort order for sidebar.
+  */
+  static get DEFINITION_FILES() {
+    return [
+      'core-concepts.js',
+      'layout.js',
+      'flexbox.js',
+      'grid.js',
+      'spacing.js',
+      'sizing.js',
+      'typography.js',
+      'backgrounds.js',
+      'borders.js',
+      'effects.js',
+      'tables.js',
+      'transitions-and-animation.js',
+      'transforms.js',
+      'interactivity.js',
+      'svg.js',
+      'accessibility.js',
+      'box-alignment',
+    ]
+  }
+
+  /*
+    Gets Tailwind CSS class definitions in the form of an array Category objects,
+    each containing an array of SubCategory objects with an array of Tailwind
+    UtilityClass objects. The Tailwind UtilityClass objects have a label, detail,
+    and documentation property.
+  */
   get defintions() {
     return this._definitions
   }
 
+  /*
+    Loads all definition files defined in the Configuration.DEFINITION_FILES constant.
+  */
   async loadDefinitions() {
-    await this.getVersionDefinitionFiles()
-
-    this._definition_files.forEach(definition => {
+    Configuration.DEFINITION_FILES.forEach(definition => {
       this._definitions = [...this._definitions, require(`../Definitions/${Configuration.VERSION}/${definition}`)]
     })
 
     return
-  }
-
-  async getVersionDefinitionFiles() {
-    let path        = nova.extension.path + '/Definitions/' + Configuration.VERSION
-    let definitions = nova.fs.listdir(path)
-
-    definitions = definitions.filter((definition) => {
-      if (nova.fs.stat(path + '/' + definition).isFile()) {
-        return definition
-      }
-    })
-
-    this._definition_files = definitions
-
-    return
-  }
-
-  async loadTailwindConfig() {
-
-  }
-
-  async searchForConfigFile() {
-
   }
 }
