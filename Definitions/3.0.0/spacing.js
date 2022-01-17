ad9cfc3e6119c3ec3358da6af3e4c72288a3e0fe
Jason Platts <plattsjm@icloud.com>
2022-01-17 15:38:21 -0500

    Refactored spacing classes.

--- Definitions/3.0.0/spacing.js
+++ Definitions/3.0.0/spacing.js
@@ -5,116 +5,96 @@ exports.twClasses = function twClasses(config) {
 
   let padding = []
 
-  for(let i = 0; i < config.scales.DEFAULT_SPACING_SCALE.length; i++) {
+  config.scales.DEFAULT_SPACING_SCALE.forEach(scale => {
     padding.push(
       {
-        label:         `p-${config.scales.DEFAULT_SPACING_SCALE[i].name}`,
-        detail:        `padding: ${config.scales.DEFAULT_SPACING_SCALE[i].value};`,
-        documentation: `Set the padding on all sides of an element to ${config.scales.DEFAULT_SPACING_SCALE[i].value}.`
+        label:         `p-${scale.name}`,
+        detail:        `padding: ${scale.value};`,
+        documentation: `Set the padding on all sides of an element to ${scale.value}.`
+      },
+      {
+        label:         `px-${scale.name}`,
+        detail:        `padding-left: ${scale.value}; padding-right: ${scale.value};`,
+        documentation: `Set the horizontal padding of an element to ${scale.value}.`
+      },
+      {
+        label:         `py-${scale.name}`,
+        detail:        `padding-top: ${scale.value}; padding-bottom: ${scale.value};`,
+        documentation: `Set the vertical padding of an element to ${scale.value}.`
+      },
+      {
+        label:         `pt-${scale.name}`,
+        detail:        `padding-top: ${scale.value};`,
+        documentation: `Set the top padding of an element to ${scale.value}.`
+      },
+      {
+        label:         `pr-${scale.name}`,
+        detail:        `padding-right: ${scale.value};`,
+        documentation: `Set the right padding of an element to ${scale.value}.`
+      },
+      {
+        label:         `pb-${scale.name}`,
+        detail:        `padding-bottom: ${scale.value};`,
+        documentation: `Set the bottom padding of an element to ${scale.value}.`
+      },
+      {
+        label:         `pl-${scale.name}`,
+        detail:        `padding-left: ${scale.value};`,
+        documentation: `Set the left padding of an element to ${scale.value}.`
       }
     )
-  }
-
-  for(let i = 0; i < config.scales.DEFAULT_SPACING_SCALE.length; i++) {
-    padding.push(
-      {
-        label:         `py-${config.scales.DEFAULT_SPACING_SCALE[i].name}`,
-        detail:        `padding-top: ${config.scales.DEFAULT_SPACING_SCALE[i].value}; padding-bottom: ${config.scales.DEFAULT_SPACING_SCALE[i].value};`,
-        documentation: `Set the vertical padding of an element to ${config.scales.DEFAULT_SPACING_SCALE[i].value}.`
-      },
-      {
-        label:         `px-${config.scales.DEFAULT_SPACING_SCALE[i].name}`,
-        detail:        `padding-left: ${config.scales.DEFAULT_SPACING_SCALE[i].value}; padding-right: ${config.scales.DEFAULT_SPACING_SCALE[i].value};`,
-        documentation: `Set the horizontal padding of an element to ${config.scales.DEFAULT_SPACING_SCALE[i].value}.`
-      }
-    )
-  }
-
-  for(let i = 0; i < config.scales.DEFAULT_SPACING_SCALE.length; i++) {
-    padding.push(
-      {
-        label:         `pt-${config.scales.DEFAULT_SPACING_SCALE[i].name}`,
-        detail:        `padding-top: ${config.scales.DEFAULT_SPACING_SCALE[i].value};`,
-        documentation: `Set the top padding of an element to ${config.scales.DEFAULT_SPACING_SCALE[i].value}.`
-      },
-      {
-        label:         `pr-${config.scales.DEFAULT_SPACING_SCALE[i].name}`,
-        detail:        `padding-right: ${config.scales.DEFAULT_SPACING_SCALE[i].value};`,
-        documentation: `Set the right padding of an element to ${config.scales.DEFAULT_SPACING_SCALE[i].value}.`
-      },
-      {
-        label:         `pb-${config.scales.DEFAULT_SPACING_SCALE[i].name}`,
-        detail:        `padding-bottom: ${config.scales.DEFAULT_SPACING_SCALE[i].value};`,
-        documentation: `Set the bottom padding of an element to ${config.scales.DEFAULT_SPACING_SCALE[i].value}.`
-      },
-      {
-        label:         `pl-${config.scales.DEFAULT_SPACING_SCALE[i].name}`,
-        detail:        `padding-left: ${config.scales.DEFAULT_SPACING_SCALE[i].value};`,
-        documentation: `Set the left padding of an element to ${config.scales.DEFAULT_SPACING_SCALE[i].value}.`
-      }
-    )
-  }
+  })
 
   /*        MARGIN CLASSES        */
 
-  function addMarginClasses(negative = '') {
-    let objectsArray = []
+  let margin = []
 
-    for(let i = 0; i < config.scales.DEFAULT_SPACING_SCALE.length; i++) {
-      let negativeSizePrefix = ''
-
-      if (negative == '-') {
-        if (config.scales.DEFAULT_SPACING_SCALE[i].value !== '0px') {
-          negativeSizePrefix = '-'
-        } else {
-          negativeSizePrefix = ''
-        }
+  config.scales.DEFAULT_SPACING_SCALE.forEach(scale => {
+    margin.push(
+      {
+        label:         `m-${scale.name}`,
+        detail:        `margin: ${scale.value};`,
+        documentation: `Set the margin on all sides of an element to ${scale.value}.`,
+        allowNegation: true
+      },
+      {
+        label:         `mx-${scale.name}`,
+        detail:        `margin-left: ${scale.value}; margin-right: ${scale.value};`,
+        documentation: `Set the horizontal margin of an element to ${scale.value}.`,
+        allowNegation: true
+      },
+      {
+        label:         `my-${scale.name}`,
+        detail:        `margin-top: ${scale.value}; margin-bottom: ${scale.value};`,
+        documentation: `Set the vertical margin of an element to ${scale.value}.`,
+        allowNegation: true
+      },
+      {
+        label:         `mt-${scale.name}`,
+        detail:        `margin-top: ${scale.value};`,
+        documentation: `Set the top margin of an element to ${scale.value}.`,
+        allowNegation: true
+      },
+      {
+        label:         `mr-${scale.name}`,
+        detail:        `margin-right: ${scale.value};`,
+        documentation: `Set the right margin of an element to ${scale.value}.`,
+        allowNegation: true
+      },
+      {
+        label:         `mb-${scale.name}`,
+        detail:        `margin-bottom: ${scale.value};`,
+        documentation: `Set the bottom margin of an element to ${scale.value}.`,
+        allowNegation: true
+      },
+      {
+        label:         `ml-${scale.name}`,
+        detail:        `margin-left: ${scale.value};`,
+        documentation: `Set the left margin of an element to ${scale.value}.`,
+        allowNegation: true
       }
-
-      objectsArray.push(
-        {
-          label:         `${negative}m-${config.scales.DEFAULT_SPACING_SCALE[i].name}`,
-          detail:        `margin: ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value};`,
-          documentation: `Set the margin on all sides of an element to ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value}.`
-        },
-        {
-          label:         `${negative}my-${config.scales.DEFAULT_SPACING_SCALE[i].name}`,
-          detail:        `margin-top: ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value}; margin-bottom: ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value};`,
-          documentation: `Set the vertical margin of an element to ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value}.`
-        },
-        {
-          label:         `${negative}mx-${config.scales.DEFAULT_SPACING_SCALE[i].name}`,
-          detail:        `margin-left: ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value}; margin-right: ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value};`,
-          documentation: `Set the horizontal margin of an element to ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value}.`
-        },
-        {
-          label:         `${negative}mt-${config.scales.DEFAULT_SPACING_SCALE[i].name}`,
-          detail:        `margin-top: ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value};`,
-          documentation: `Set the top margin of an element to ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value}.`
-        },
-        {
-          label:         `${negative}mr-${config.scales.DEFAULT_SPACING_SCALE[i].name}`,
-          detail:        `margin-right: ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value};`,
-          documentation: `Set the right margin of an element to ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value}.`
-        },
-        {
-          label:         `${negative}mb-${config.scales.DEFAULT_SPACING_SCALE[i].name}`,
-          detail:        `margin-bottom: ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value};`,
-          documentation: `Set the bottom margin of an element to ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value}.`
-        },
-        {
-          label:         `${negative}ml-${config.scales.DEFAULT_SPACING_SCALE[i].name}`,
-          detail:        `margin-left: ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value};`,
-          documentation: `Set the left margin of an element to ${negativeSizePrefix}${config.scales.DEFAULT_SPACING_SCALE[i].value}.`
-        },
-      )
-    }
-
-    return objectsArray
-  }
-
-  let margin = addMarginClasses()
-  margin     = [...margin, ...addMarginClasses('-')]
+    )
+  })
 
   margin.push(
     {
@@ -163,8 +143,15 @@ exports.twClasses = function twClasses(config) {
       {
         label:         `space-x-${scale.name}`,
         detail:        `--tw-space-x-reverse: 0; margin-right: calc(${scale.value} * var(--tw-space-x-reverse)); margin-left: calc(${scale.value} * calc(1 - var(--tw-space-x-reverse)));`,
-        documentation: 'Set horizontal space between child elements.'
-      }
+        documentation: 'Set horizontal space between child elements.',
+        allowNegation: true
+      },
+      {
+        label:         `space-y-${scale.name}`,
+        detail:        `--tw-space-y-reverse: 0; margin-top: calc(${scale.value} * calc(1 - var(--tw-space-y-reverse))); margin-bottom: calc(${scale.value} * var(--tw-space-y-reverse));`,
+        documentation: 'Set the vertical space between child elements.',
+        allowNegation: true
+      },
     )
   })
 
@@ -173,46 +160,13 @@ exports.twClasses = function twClasses(config) {
       label:         'space-x-reverse',
       detail:        '--tw-space-x-reverse: 1;',
       documentation: 'If elements are in reverse order (eg: flex-row-reverse), set the horizontal space to the correct side of an element.'
-    }
-  )
-
-  config.scales.DEFAULT_SPACING_SCALE.forEach(scale => {
-    spaceBetween.push(
-      {
-        label:         `-space-x-${scale.name}`,
-        detail:        `--tw-space-x-reverse: 0; margin-right: calc(-${scale.value} * var(--tw-space-x-reverse)); margin-left: calc(-${scale.value} * calc(1 - var(--tw-space-x-reverse)));`,
-        documentation: 'Set negative horizontal space between child elements.'
-      }
-    )
-  })
-
-  config.scales.DEFAULT_SPACING_SCALE.forEach(scale => {
-    spaceBetween.push(
-      {
-        label:         `space-y-${scale.name}`,
-        detail:        `--tw-space-y-reverse: 0; margin-top: calc(${scale.value} * calc(1 - var(--tw-space-y-reverse))); margin-bottom: calc(${scale.value} * var(--tw-space-y-reverse));`,
-        documentation: 'Set the vertical space between child elements.'
-      },
-    )
-  })
-
-  spaceBetween.push(
+    },
     {
       label:         'space-y-reverse',
       detail:        '--tw-space-y-reverse: 1;',
       documentation: 'If elements are in reverse order (eg: flex-col-reverse), set the vertical space to the correct side of an element.'
     }
   )
-
-  config.scales.DEFAULT_SPACING_SCALE.forEach(scale => {
-    spaceBetween.push(
-      {
-        label:         `-space-y-${scale.name}`,
-        detail:        `--tw-space-y-reverse: 0; margin-top: calc(-${scale.value} * calc(1 - var(--tw-space-y-reverse))); margin-bottom: calc(-${scale.value} * var(--tw-space-y-reverse));`,
-        documentation: 'Set the vertical space between child elements.'
-      },
-    )
-  })
 
   return {
     spacing: {
