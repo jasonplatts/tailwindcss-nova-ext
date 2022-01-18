31fb9058cee9907145bcc666020d33cc0c271ac0
Jason Platts <plattsjm@icloud.com>
2022-01-18 16:27:18 -0500

    Updated svg classes.

--- Definitions/3.0.0/svg.js
+++ Definitions/3.0.0/svg.js
@@ -1,25 +1,71 @@
 'use strict'
 
+const FUNCTIONS = require('../../Scripts/functions.js')
+
 exports.twClasses = function twClasses(config) {
   /*        FILL CLASSES        */
 
   let fill = [
     {
+      label:         'fill-inherit',
+      detail:        'fill: inherit;',
+      documentation: 'Set the fill color of an SVG element to that of the parent element.'
+    },
+    {
       label:         'fill-current',
       detail:        'fill: currentColor;',
-      documentation: 'Set the fill color of an SVG to the current text color.'
+      documentation: 'Set the fill color of an SVG element to the currentColor.'
+    },
+    {
+      label:         'fill-transparent',
+      color:         new Color('rgb', [0, 0, 0, 0]),
+      detail:        'fill: transparent;',
+      documentation: 'Set the fill color of an SVG element to transparent.'
     }
   ]
+
+  config.colors.forEach(color => {
+    fill.push(
+      {
+        label:         `fill-${color.name}`,
+        color:         FUNCTIONS.convertHexToRgbColorObject(color.hex),
+        detail:        `fill: #${color.hex};`,
+        documentation: `Set the fill color of an SVG element to #${color.hex}.`
+      }
+    )
+  })
 
   /*        STROKE CLASSES        */
 
   let stroke = [
     {
+      label:         'stroke-inherit',
+      detail:        'stroke: inherit;',
+      documentation: 'Set the stroke color of an SVG element to that of the parent element.'
+    },
+    {
       label:         'stroke-current',
       detail:        'stroke: currentColor;',
-      documentation: 'Set the stroke color of an SVG to the current text color.'
+      documentation: 'Set the stroke color of an SVG element to the currentColor.'
+    },
+    {
+      label:         'stroke-transparent',
+      color:         new Color('rgb', [0, 0, 0, 0]),
+      detail:        'stroke: transparent;',
+      documentation: 'Set the stroke color of an SVG element to transparent.'
     }
   ]
+
+  config.colors.forEach(color => {
+    stroke.push(
+      {
+        label:         `stroke-${color.name}`,
+        color:         FUNCTIONS.convertHexToRgbColorObject(color.hex),
+        detail:        `stroke: #${color.hex};`,
+        documentation: `Set the stroke color of an SVG element to #${color.hex}.`
+      }
+    )
+  })
 
   /*        STROKE WIDTH CLASSES        */
 
