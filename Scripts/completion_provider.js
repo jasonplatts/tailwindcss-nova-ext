a1aadf53860723dd297257d8d172c11de60da11c
Emran Mashhadi Ramezan <Emran@pharmcademy.com>
2022-02-02 19:03:02 +0000

    Preventing Autocompletion in PHP context

--- Scripts/completion_provider.js
+++ Scripts/completion_provider.js
@@ -105,7 +105,15 @@ exports.CompletionProvider = class CompletionProvider {
       // console.log(context.selectors[1].string)
       // console.log(context.selectors[2].string)
       // console.log(context.selectors[0].matches('html'))
-
+      
+      //Prevent completion in php context
+      if (
+        context.selectors[0].string === 'php.string.single-quoted.delimiter' ||
+        'php.string.double-quoted.delimiter'
+      ) {
+        return true;
+      }
+      
       // Allow completions if context selectors is a HTML class attribute value.
       if (context.selectors[0].matches('html.tag.attribute.class.value.double-quoted')) { return false }
 
